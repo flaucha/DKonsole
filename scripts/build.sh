57d01602b3b54d3195e0422157bec13a8db69dfb
flaucha <flaucha@gmail.com>
2025-11-27 21:47:28 -0300

    chore: update code before build 1.2.6-test-2

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,8 +11,8 @@ else
     VERSION="1.1.9"
 fi
 
-# Use test version for testing (add -test-1 suffix)
-TEST_VERSION="${VERSION}-test-1"
+# Use test version for testing (add -test-2 suffix)
+TEST_VERSION="${VERSION}-test-2"
 
 echo "=========================================="
 echo "🔨 DKonsole Build v${TEST_VERSION}"
